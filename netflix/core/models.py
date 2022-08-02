6ec08efa309f2dc237362b8d50f7802518c56597
Bhavesh <bhaveshkadian.rtk@gmail.com>
2022-08-02 19:05:04 +0530

    modeladded

--- netflix/core/models.py
+++ netflix/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+AGE_CHOICES = (
+    ('ALL','ALL'),
+    ('Kids','Kids');
+
+)
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyFiels('Profile',null=True,blank =True)
+
+class Profile(models.Model):
+    nume=models.CharField(max_length = 225)
+    age_limit = models.CharField(max_length =10,choices = AGE_CHOICES)
+    uuid = models.UUIDField(default=uuid.uuid4 )
--- netflix/netflix/urls.py
+++ netflix/netflix/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from argparse import Namespace
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import urls
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('core.urls',name='core'))
 ]
 
 if settings.DEBUG:
